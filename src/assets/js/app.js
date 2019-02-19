2cac86e381ac6a555f00d430e1c2dc018e503125
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-02-19 18:32:24 +0300

    gallery for wp content

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,4 +8,29 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
+
+//
+// adapt WP block gallery to match Foundation Orbit protocol
+//
+
+  // create orbit wrapper before wp gallery
+$(".wp-block-gallery").each(function() {
+  $(this).prev().after('<div class="srp-gallery orbit" role="region" data-orbit=""><div class="orbit-wrapper"><div class="orbit-controls"><button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button> <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button></div></div></div>');
+});
+
+// put wp gallery inside of the orbit wrapper
+$(".srp-gallery").each(function () {
+  var wpGallery = $(this).next();
+
+  $(this).find(".orbit-wrapper").append(wpGallery);
+});
+
+// readjust classes
+$(".srp-gallery ul").removeClass("wp-block-gallery").addClass("orbit-container");
+
+$(".srp-gallery li").removeClass("blocks-gallery-item").addClass("orbit-slide");
+$(".srp-gallery figure").addClass("orbit-figure");
+$(".srp-gallery img").addClass("orbit-image");
+$(".srp-gallery figcaption").addClass("orbit-caption");
+
 $(document).foundation();
