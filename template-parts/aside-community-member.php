d93189332cd35c6843c19665ffe35b0275d8e55e
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-02-28 01:23:53 +0300

    fixed members count on community pages

--- template-parts/aside-community-member.php
+++ template-parts/aside-community-member.php
@@ -2,7 +2,7 @@
 <h2 class="<?php echo("group" == srp_get_post_format() ? 'active' : ''); ?>">
   GROUPS
 </h2>
-<?php $groups = get_posts(array("category_name" => "groups")); ?>
+<?php $groups = get_posts(array("category_name" => "groups", 'posts_per_page'=> -1)); ?>
 <div class="groups">
   <?php foreach($groups as $group): ?>
     <a class="group <?php echo srp_active_class($group) ?>" href="<?php echo get_permalink($group); ?>"><?php echo trim($group->post_title); ?></a>
@@ -13,7 +13,7 @@
 <h2 class="<?php echo("person" == srp_get_post_format() ? 'active' : ''); ?>">
   People
 </h2>
-<?php $people = get_posts(array("category_name" => "people")); ?>
+<?php $people = get_posts(array("category_name" => "people", 'posts_per_page'=> -1)); ?>
 <div class="people">
   <?php foreach($people as $person): ?>
     <a class="person <?php echo srp_active_class($person) ?>" href="<?php echo get_permalink($person); ?>"><?php echo trim($person->post_title); ?></a>
