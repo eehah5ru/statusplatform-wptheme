e6a79df12e21697596a00220b0dc2a0efa9a5445
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-02-28 02:41:22 +0300

    fixed youtube link

--- header.php
+++ header.php
@@ -47,7 +47,7 @@
 
             <nav class="bottom-links">
               <div class="bottom-links-container">
-                <h3><a href="https://www.facebook.com/Status-Research-Platform-282185609116550/" target="_blank">FB</a> | <a hre="https://www.youtube.com/channel/UCqk6xLP74p08ZjNYZuECHwg" target="_blank">Youtube</a></h3>                
+                <h3><a href="https://www.facebook.com/Status-Research-Platform-282185609116550/" target="_blank">FB</a> | <a href="https://www.youtube.com/channel/UCqk6xLP74p08ZjNYZuECHwg" target="_blank">Youtube</a></h3>                
                 <h3><a href="mailto:info@statusproject.net"><?php pll_e('Contact us'); ?></a></h3>
                 <!-- <h3><a><?php pll_e('Disclaimer'); ?></a></h3> -->
                 <h3><a href="<?php srp_the_permalink_by_path('terms-of-use'); ?>"><?php pll_e('Terms of Use'); ?></a></h3>
