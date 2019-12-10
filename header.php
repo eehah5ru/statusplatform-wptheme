52cc77d6fd62a6a9968ad37e3c6ddf76c695ffe2
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-12-10 14:25:23 +0300

    add google analytics tracking code

--- header.php
+++ header.php
@@ -17,6 +17,17 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                               m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-154356512-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <!-- End Google Analytics -->
 
 	  <?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 		  <?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
