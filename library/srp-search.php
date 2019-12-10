e689c28b84da6d2716eb6bed87ec079388f32d7a
Nicola Spesivtsev <nicola.spesivcev@gmail.com>
2019-12-10 14:10:13 +0300

    remove limit of search results

---
+++ library/srp-search.php
@@ -0,0 +1,12 @@
+<?php
+// customize search results
+function srp_search_customize($query) {
+  if ($query->is_search) {
+    // disable limit
+    $query->set('posts_per_page', "-1");
+  }
+
+  return $query;
+}
+add_filter( 'pre_get_posts','srp_search_customize' );
+?>
